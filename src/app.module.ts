f0639c435e6ac7f4f3384bdcf2cfc2a8bfd8c590
Mateus Sousa <mateusfj8144@gmail.com>
2025-12-11 17:03:08 -0300

    feat(modules): cria os moodulos iniciais de leads e properties

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LeadsModule } from './modules/leads/leads.module';
+import { PropertiesModule } from './modules/properties/properties.module';
 
 @Module({
-  imports: [],
+  imports: [LeadsModule, PropertiesModule],
   controllers: [AppController],
   providers: [AppService],
 })
